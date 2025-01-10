079e43811755e3f5a70b7b48200a219ed4c6a155
Saurabh Bhatt <bhattsourav4@gmail.com>
2025-01-11 00:33:33 +0530

    utils code discussion

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors"
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+//app.use is used for configuration or middleware
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({limit: "16kb"}))                 // used for json data
+app.use(express.urlencoded({extended: true, limit: "16kb"}))   // used for url data
+app.use(express.static("public"))               // used for store public data like favicon, images and files
+app.use(cookieParser())                     // read and set cookie in user browser
+
+ 
+
+export { app }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ import connectDB from "./db/index.js";
 
 
 connectDB()
+.then(()    =>{
+    app.listen(process.env.PORT || 8000, () =>  {
+        console.log(` Server is running at port:
+             ${process.env.PORT}`);
+
+    })
+}
+
+)
+.catch(err)
+{
+    console.log("MONGODB CONNECTION FAILED !!!", err)
+}
 
 
 
